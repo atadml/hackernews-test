cef9fd7f5d84cd83052488721429e95168d11751
atad_name <atad_email@email.com>
2024-05-21 22:17:31 +0000

    Edit /hackernews-test/improvado_results.sql by new promt: compare amount of stories this week vs last week un sql

--- improvado_results.sql
+++ improvado_results.sql
@@ -1,5 +1,1 @@
-SELECT
-  *
-FROM fh-bigquery.hackernews.stories
-WHERE
-  score = (SELECT MAX(score) FROM fh-bigquery.hackernews.stories)
+Error: Output DSL not found or unsupported. Please make sure to end the query with 'in <DSL>' where DSL is one of: DBT, LookML, SQL, LKML
